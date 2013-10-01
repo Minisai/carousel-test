428fe8c64b4ecd8e802e14e554ce3028558db8d7
Anton Dziga <antonjyha@gmail.com>
2013-10-01 13:35:32 +0300

    add config.ru

--- Gemfile
+++ Gemfile
@@ -1,6 +1,4 @@
-source 'https://rubygems.org'
-
 source "https://rubygems.org"
+
 ruby "2.0.0"
 gem 'sinatra', '1.1.0'
-#gem 'sinatra-assetpack'
--- myapp.rb
+++ myapp.rb
@@ -1,25 +1,5 @@
 require 'sinatra'
-#require 'sinatra/assetpack'
 
 get '/' do
-  send_file File.join(settings.public_folder, 'index.html')
+  send_file 'public/index.html'
 end
-
-#class App < Sinatra::Base
-#  register Sinatra::AssetPack
-#  assets do
-#    js :application, [
-#        '/js/jquery.min.js',
-#    ]
-#
-#    css :application, [
-#    ]
-#  end
-#
-#  get '/' do
-#    send_file File.join(settings.public_folder, 'index.html')
-#  end
-#end
-
-
-
